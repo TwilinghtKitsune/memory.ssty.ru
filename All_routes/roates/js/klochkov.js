d914e6eab8c3a0a39d2a6c10341ab733036a2940
AnastasiaZay <zajch.anastasia@yandex.ru>
2022-07-02 15:44:44 +0400

    Глобальные правки 5
    Оптимизация изображений, удаление лишних, оптимизация расположения, редактирование страниц с маршрутами

--- All_routes/roates/js/klochkov.js
+++ All_routes/roates/js/klochkov.js
@@ -1,7 +1,7 @@
 // json
 let points = '{"points":[';
-let p0 = '{ "coordinates1":51.526946, "coordinates2":45.970917, "balloonContentHeader": "images/holzunov/table2.JPG",  "balloonContentBody": "<div>Школа № 97.<br>На территории школы установлен памятник Клочкову В.Г.</div>", "hintContent": "Школа №97" },';
-let p1 = '{ "coordinates1":51.524978, "coordinates2":45.974458, "balloonContentHeader": "images/holzunov/table2.JPG",  "balloonContentBody": "<div>Стела Героя Советского Союза <br> Клочкова В. Г.</div>", "hintContent": "Стела им.Клочкова В.Г." },';
+let p0 = '{ "coordinates1":51.526946, "coordinates2":45.970917, "balloonContentHeader": "../../img/routes_img/holzunov/table2.JPG",  "balloonContentBody": "<div>Школа № 97.<br>На территории школы установлен памятник Клочкову В.Г.</div>", "hintContent": "Школа №97" },';
+let p1 = '{ "coordinates1":51.524978, "coordinates2":45.974458, "balloonContentHeader": "../../img/routes_img/holzunov/table2.JPG",  "balloonContentBody": "<div>Стела Героя Советского Союза <br> Клочкова В. Г.</div>", "hintContent": "Стела им.Клочкова В.Г." },';
 
 points += p0 + p1 + "]}";
 console.log(points);
